51c824e10c94f7eb6a5a2c7e3ef274b8dbeed6cc
Dominik Brzozowski <dodopl15333@gmail.com>
2022-01-21 19:09:42 +0100

    Edycja folderu css

--- css/skrypt.js
+++ css/skrypt.js
@@ -1,1 +1,6 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
